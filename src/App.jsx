4b356939200ed121790073618a57b891a5d25898
Isabel <IsabelChong.CYJI@gmail.com>
2023-05-05 06:26:41 +0000

    Update spline position

--- src/App.jsx
+++ src/App.jsx
@@ -63,15 +63,17 @@ const Wrapper = styled.div`
   position: relative;
   width: 100%;
   height: 100%;
+  overflow-x: hidden;
 
   .spline {
     position: absolute;
     margin: 0;
     top: 0;
-    left: 0;
+    right: 0;
+    transform: translateX(400px);
 
-    @media (max-width: 1024px) {
-      transform: scale(0.8) translateX(200px);
+    @media (max-width: 1124px) {
+      transform: scale(0.8) translateX(100px);
       transform-origin: top;
     }
     @media (max-width: 800px) {
@@ -116,9 +118,10 @@ const Content = styled.div`
     @media (max-width: 800px) {
       font-size: 40px;
       max-width: 300px;
+      padding-top: 250px;
     }
     @media (max-width: 600px) {
-      padding-top: 250px;
+      padding-top: 300px;
     }
   }
 
@@ -189,6 +192,10 @@ const Menu = styled.ul`
       :hover {
         border: 1px solid rgba(255, 255, 255, 0.2);
       }
+    }
+    
+    button {
+      align-items: left;
     }
   }
   
